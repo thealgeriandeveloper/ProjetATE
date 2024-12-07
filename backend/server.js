72506b4e0a18b4826e62ba4297ec5284104450a3
Sh4ytan <125776575+Sh4ytan@users.noreply.github.com>
2024-12-06 21:43:46 -0500

    config pour host

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const { createObjectCsvWriter } = require("csv-writer");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Dynamic port for deployment
 
 // Middleware : servir les fichiers statiques depuis "frontend"
 app.use(express.static(path.join(__dirname, "../frontend")));
